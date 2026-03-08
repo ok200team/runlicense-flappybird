8737574cfcf41aa6288c3c88068dc1238e01f0f9
Paul Grimes <paul@ok200.net>
2026-03-08 15:13:45 +1100

    0.1.20

--- my_wasm_thingy_bg.wasm.d.ts
+++ my_wasm_thingy_bg.wasm.d.ts
@@ -3,12 +3,12 @@
 export const memory: WebAssembly.Memory;
 export const activate: (a: number, b: number, c: number, d: number) => void;
 export const wasm_bindgen__closure__destroy__ha0d3a3f7e7ebf7b8: (a: number, b: number) => void;
-export const wasm_bindgen__closure__destroy__h6a4c256389903c9d: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h406846795877efe7: (a: number, b: number) => void;
 export const wasm_bindgen__closure__destroy__h8fc8786b4abf673e: (a: number, b: number) => void;
 export const wasm_bindgen__convert__closures_____invoke__hf60ede254a8638fa: (a: number, b: number, c: any) => [number, number];
-export const wasm_bindgen__convert__closures_____invoke__h26c5df9390e95392: (a: number, b: number, c: any) => void;
-export const wasm_bindgen__convert__closures_____invoke__h26c5df9390e95392_2: (a: number, b: number, c: any) => void;
-export const wasm_bindgen__convert__closures_____invoke__h26c5df9390e95392_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1b0dbb7f2042f562: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1b0dbb7f2042f562_2: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1b0dbb7f2042f562_3: (a: number, b: number, c: any) => void;
 export const wasm_bindgen__convert__closures_____invoke__hb26893cad6ad8ded: (a: number, b: number) => void;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
