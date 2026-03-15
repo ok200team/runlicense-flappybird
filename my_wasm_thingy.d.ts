8de910f020f1b3bf75a29ac10e24792b9e9558a8
Paul Grimes <paul@ok200.net>
2026-03-15 12:56:04 +1100

    0.3.2

--- my_wasm_thingy.d.ts
+++ my_wasm_thingy.d.ts
@@ -18,11 +18,11 @@ export interface InitOutput {
     readonly activate: (a: number, b: number) => void;
     readonly wasm_bindgen__closure__destroy__h3ed56fe3f612d2b8: (a: number, b: number) => void;
     readonly wasm_bindgen__closure__destroy__h5b569a9b0c99a6ce: (a: number, b: number) => void;
-    readonly wasm_bindgen__closure__destroy__h5c17103ffb908a6d: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h37c8dcf88878745d: (a: number, b: number) => void;
     readonly wasm_bindgen__convert__closures_____invoke__h26e23bd7929d5711: (a: number, b: number, c: any) => [number, number];
-    readonly wasm_bindgen__convert__closures_____invoke__h3888af13b9e6f504: (a: number, b: number, c: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h3888af13b9e6f504_3: (a: number, b: number, c: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h3888af13b9e6f504_4: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h214222b30e2267f6: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h214222b30e2267f6_3: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h214222b30e2267f6_4: (a: number, b: number, c: any) => void;
     readonly wasm_bindgen__convert__closures_____invoke__hf1aa2e30d6244af6: (a: number, b: number) => void;
     readonly __wbindgen_malloc: (a: number, b: number) => number;
     readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
